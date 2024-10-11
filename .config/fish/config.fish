5874699c119c9c30be3e9f10e72cd7c4f744baf5
scored wrench <maximizemanhood@outlook.com>
2024-10-11 21:34:45 +0800

    feat(fish): automatically start Sway on TTY login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,12 @@
+if status is-interactive
+    if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" -eq 1
+        exec sway
+    end
+end
+
 set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
 set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
+
+set fish_greeting
 
 starship init fish | source

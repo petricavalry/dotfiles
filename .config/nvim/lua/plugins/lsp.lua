724524bf189bafd963a09eca163a9ca7e76d32ac
scored wrench <maximizemanhood@outlook.com>
2024-10-11 21:42:18 +0800

    fix(neovim): selection items in completion menu

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,9 @@ return {
         config = function()
             local cmp = require("cmp")
             cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
                 }),

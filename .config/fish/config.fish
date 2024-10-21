4156f4d2741cda253b8ad1b2f2f76da8c547cca7
scored wrench <maximizemanhood@outlook.com>
2024-10-21 18:10:06 +0800

    revert: automatically start Sway on TTY login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
-if status is-interactive
-    if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" -eq 1
-        exec sway
-    end
-end
-
 alias dotfiles "git --git-dir=\"$HOME/.dotfiles\" --work-tree=\"$HOME\""
 
 function fish_prompt
